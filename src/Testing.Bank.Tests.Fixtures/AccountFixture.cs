69288a4a2194359a1abc813d0456057c19a8e7a0
Sergio Barriel <sergiobarriel@gmail.com>
2021-10-16 12:24:59 +0200

    Added new test battery (fixture)

---
+++ src/Testing.Bank.Tests.Fixtures/AccountFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing.Bank.Tests.Fixtures
+{
+    public class AccountFixture : IDisposable
+    {
+        public Account Account;
+
+        public AccountFixture()
+        {
+            Account = new Account(new User("Rick Sánchez", 70), new List<Transaction>()
+            {
+                new Transaction(TransactionType.Deposit, 100, $"Deposit 100 $"),
+                new Transaction(TransactionType.Deposit, 200, $"Deposit 100 $"),
+                new Transaction(TransactionType.Withdraw, 150, $"Withdraw 100 $"),
+                new Transaction(TransactionType.Deposit, 100, $"Deposit 100 $"),
+                new Transaction(TransactionType.Deposit, 100, $"Deposit 100 $"),
+            });
+        }
+
+        public void Dispose()
+        {
+            Account = null;
+        }
+    }
+}
---
+++ src/Testing.Bank.Tests.Fixtures/AccountShould.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Testing.Bank.Tests.Fixtures
+{
+    public class AccountShould : IClassFixture<AccountFixture>
+    {
+        private readonly AccountFixture _accountFixture;
+
+        public AccountShould(AccountFixture accountFixture)
+        {
+            _accountFixture = accountFixture;
+        }
+
+
+        [Fact]
+        public void AccountDeposit_ReturnsRightBalance()
+        {
+            // Arrange
+            var startingBalance = _accountFixture.Account.GetBalance();
+            var amount = 50;
+
+            // Act 
+            _accountFixture.Account.Deposit(amount, "Deposit {amount} $");
+
+            // Assert
+            Assert.Equal(startingBalance + amount, _accountFixture.Account.GetBalance());
+        }
+
+        [Fact]
+        public void AccountWithdrawAll_ReturnsZeroBalance()
+        {
+            // Arrange
+            var startingBalance = _accountFixture.Account.GetBalance();
+            var amount = startingBalance;
+
+            // Act 
+            _accountFixture.Account.Withdraw(amount, "Total withdraw {amount} $");
+
+            // Assert
+            Assert.Equal(0, _accountFixture.Account.GetBalance());
+        }
+
+        [Fact]
+        public void AccountExcessiveWithdrawal_ThrowsException()
+        {
+            // Arrange
+            var startingBalance = _accountFixture.Account.GetBalance();
+            var amount = startingBalance;
+
+            // Act
+            Action action = () =>
+            {
+                _accountFixture.Account.Withdraw(amount + 1, "Excessive withdraw {amount} $");
+            };
+
+            // Assert
+            Assert.Throws<Exception>(action);
+        }
+    }
+}
